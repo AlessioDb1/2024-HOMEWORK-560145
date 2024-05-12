5aa6a6adbae8bf7a54f8638ede7a66dd51b68de9
Alessio Di Biagio <166547217+AlessioDb1@users.noreply.github.com>
2024-05-12 13:02:56 +0200

    Update Labirinto.java

--- src/it/uniroma3/diadia/ambienti/Labirinto.java
+++ src/it/uniroma3/diadia/ambienti/Labirinto.java
@@ -1,6 +1,4 @@
 package it.uniroma3.diadia.ambienti;
-
-import it.uniroma3.diadia.attrezzi.Attrezzo;
 /**
  * Classe Labirinto - labirinto di stanze in cui si muove il giocatore.
  * Questa classe modella il labirinto in cui dovrà muoversi il giocatore,
@@ -16,46 +14,7 @@ public class Labirinto {
 	/**
 	 * Crea tutte le stanze e le porte di collegamento
 	 */
-	public Labirinto init() {
-
-		/* crea gli attrezzi */
-		Attrezzo lanterna = new Attrezzo("lanterna",3);
-		Attrezzo osso = new Attrezzo("osso",1);
-		Attrezzo pass = new Attrezzo ("passpartout",0);
-		Attrezzo spada = new Attrezzo("spada", 1);
-
-		/* crea stanze del labirinto */
-		Stanza atrio = new Stanza("Atrio");
-		Stanza aulaN11 = new StanzaBloccata("Aula N11", "est", "passpartout");
-		Stanza aulaN10 = new StanzaMagica("Aula N10");
-		Stanza laboratorio = new StanzaBuia("Laboratorio Campus","lanterna");
-		Stanza biblioteca = new Stanza("Biblioteca");
-
-		/* collega le stanze */
-		atrio.impostaStanzaAdiacente("nord", biblioteca);
-		atrio.impostaStanzaAdiacente("est", aulaN11);
-		atrio.impostaStanzaAdiacente("sud", aulaN10);
-		atrio.impostaStanzaAdiacente("ovest", laboratorio);
-		aulaN11.impostaStanzaAdiacente("est", laboratorio);
-		aulaN11.impostaStanzaAdiacente("ovest", atrio);
-		aulaN10.impostaStanzaAdiacente("nord", atrio);
-		aulaN10.impostaStanzaAdiacente("est", aulaN11);
-		aulaN10.impostaStanzaAdiacente("ovest", laboratorio);
-		laboratorio.impostaStanzaAdiacente("est", atrio);
-		laboratorio.impostaStanzaAdiacente("ovest", aulaN11);
-		biblioteca.impostaStanzaAdiacente("sud", atrio);
-
-		/* pone gli attrezzi nelle stanze */
-		aulaN10.addAttrezzo(lanterna);
-		atrio.addAttrezzo(osso);
-		laboratorio.addAttrezzo(pass);
-		aulaN11.addAttrezzo(spada);
-
-		// il gioco comincia nell'atrio
-		this.stanzaIniziale = atrio;  
-		this.stanzaVincente = biblioteca;
-		return this;
-	}
+	
 	
 	public Stanza getStanzaVincente() {
 		return stanzaVincente;
@@ -65,7 +24,7 @@ public class Labirinto {
 		return this.stanzaIniziale;
 	}
 	
-	//Metodi ausiliari per testing
+	
 	public void setStanzaIniziale(Stanza stanza) {
 		this.stanzaIniziale = stanza;
 	}
