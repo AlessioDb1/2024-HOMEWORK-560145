57bebad3fcbc71070d0dbec811b0d7d5b64ca73d
Alessio Di Biagio <166547217+AlessioDb1@users.noreply.github.com>
2024-05-17 14:52:15 +0200

    Update BorsaTest.java
    Aggiunta dei test di unità per i nuovi metodi implementati.

--- Test.java/it/uniroma3/diadiaTest/giocatoreTest/BorsaTest.java
+++ Test.java/it/uniroma3/diadiaTest/giocatoreTest/BorsaTest.java
@@ -4,6 +4,11 @@ import it.uniroma3.diadia.giocatore.Borsa;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+
+
 import org.junit.jupiter.api.Test;
 
 
@@ -72,4 +77,112 @@ class BorsaTest {
 		assertNull(creaBorsa(1,"Spada").removeAttrezzo("Lancia"));
 		assertEquals(1,this.borsa.getAttrezzi().size());
 	}
+	@Test
+	void testGetContenutoOrdinatoPerPesoConBorsaOrdinata() {
+		this.borsa = new Borsa(20);
+		borsa.addAttrezzo(osso1);
+		borsa.addAttrezzo(new Attrezzo("Spada", 2));
+		borsa.getContenutoOrdinatoPerPeso();
+		assertEquals("osso", this.borsa.getAttrezzi().get(0).getNome());
+		assertEquals("Spada", this.borsa.getAttrezzi().get(1).getNome());
+	}
+	@Test
+	void testGetContenutoOrdinatoPerPesoConBorsaNonOrdinato() {
+		this.borsa = new Borsa(20);
+		borsa.addAttrezzo(new Attrezzo("Spada", 2));
+		borsa.addAttrezzo(osso1);
+		borsa.getContenutoOrdinatoPerPeso();
+		assertEquals("osso", this.borsa.getAttrezzi().get(0).getNome());
+		assertEquals("Spada", this.borsa.getAttrezzi().get(1).getNome());
+	}
+	@Test
+	void testGetContenutoOrdinatoPerPesoConBorsaConAttrezziStessoPeso() {
+		this.borsa = new Borsa(20);
+		borsa.addAttrezzo(new Attrezzo("spada", 1));
+		borsa.addAttrezzo(osso1);
+		borsa.getContenutoOrdinatoPerPeso();
+		assertEquals("osso", this.borsa.getAttrezzi().get(0).getNome());
+		assertEquals("spada", this.borsa.getAttrezzi().get(1).getNome());
+	}
+	@Test
+	void testGetContenutoOrdinatoPerPesoConBorsaNonOrdinataEMoltiAttrezzi() {
+		this.borsa = new Borsa(20);
+		borsa.addAttrezzo(new Attrezzo("spada", 2));
+		borsa.addAttrezzo(osso1);
+		borsa.addAttrezzo(new Attrezzo("lancia", 5));
+		borsa.addAttrezzo(new Attrezzo("pass", 0));
+		borsa.addAttrezzo(new Attrezzo("lanterna", 4));
+		borsa.getContenutoOrdinatoPerPeso();
+		assertEquals("pass", this.borsa.getAttrezzi().get(0).getNome());
+		assertEquals("osso", this.borsa.getAttrezzi().get(1).getNome());
+		assertEquals("spada", this.borsa.getAttrezzi().get(2).getNome());
+		assertEquals("lanterna", this.borsa.getAttrezzi().get(3).getNome());
+		assertEquals("lancia", this.borsa.getAttrezzi().get(4).getNome());
+	}
+	@Test
+	void testGetContenutoOrdinatoPerNomeConBorsaOrdinata() {
+		this.borsa = new Borsa(20);
+		borsa.addAttrezzo(osso1);
+		borsa.addAttrezzo(new Attrezzo("spada", 2));
+		Set<Attrezzo> s= borsa.getContenutoOrdinatoPerNome();
+		ArrayList<Attrezzo> a = new ArrayList<Attrezzo>(s);
+		assertEquals("osso", a.get(0).getNome());
+		assertEquals("spada", a.get(1).getNome());
+	}
+	@Test
+	void testGetContenutoOrdinatoPerNomeConBorsaNonOrdinata() {
+		this.borsa = new Borsa(20);
+		borsa.addAttrezzo(new Attrezzo("spada", 2));
+		borsa.addAttrezzo(osso1);
+		Set<Attrezzo> s= borsa.getContenutoOrdinatoPerNome();
+		ArrayList<Attrezzo> a = new ArrayList<Attrezzo>(s);
+		assertEquals("osso", a.get(0).getNome());
+		assertEquals("spada", a.get(1).getNome());
+	}
+	@Test
+	void testGetContenutoOrdinatoPerNomeConAttrezziStessoNome() {
+		this.borsa = new Borsa(20);
+		borsa.addAttrezzo(new Attrezzo("spada", 2));
+		borsa.addAttrezzo(new Attrezzo("spada", 1));
+		Set<Attrezzo> s= borsa.getContenutoOrdinatoPerNome();
+		ArrayList<Attrezzo> a = new ArrayList<Attrezzo>(s);
+		assertEquals(1, a.get(0).getPeso());
+		assertEquals(2, a.get(1).getPeso());
+	}
+	
+	@Test
+	void testGetContenutoOrdinatoPerNomeConBorsaNonOrdinataEMoltiattrezzi() {
+		this.borsa = new Borsa(20);
+		borsa.addAttrezzo(new Attrezzo("spada", 2));
+		borsa.addAttrezzo(osso1);
+		borsa.addAttrezzo(new Attrezzo("lancia", 5));
+		borsa.addAttrezzo(new Attrezzo("pass", 0));
+		borsa.addAttrezzo(new Attrezzo("lanterna", 4));
+		Set<Attrezzo> s= borsa.getContenutoOrdinatoPerNome();
+		ArrayList<Attrezzo> a = new ArrayList<Attrezzo>(s);
+		assertEquals("lancia", a.get(0).getNome());
+		assertEquals("lanterna", a.get(1).getNome());
+		assertEquals("osso", a.get(2).getNome());
+		assertEquals("pass", a.get(3).getNome());
+		assertEquals("spada", a.get(4).getNome());
+	}
+	@Test
+	void testGetContenutoRaggruppatoPerPesoConUnSoloGruppo() {
+		this.borsa = new Borsa(20);
+		borsa.addAttrezzo(osso1);
+		Map<Integer, Set<Attrezzo>> m = borsa.getContenutoRaggruppatoPerPeso();
+		assertEquals("osso",new ArrayList<Attrezzo>(m.get(1)).get(0).getNome());
+	}
+	@Test
+	void testGetContenutoRaggruppatoPerPesoConPiùGruppi() {
+		this.borsa = new Borsa(20);
+		borsa.addAttrezzo(osso1);
+		borsa.addAttrezzo(new Attrezzo("spada", 2));
+		borsa.addAttrezzo(new Attrezzo("lancia", 3));
+		Map<Integer, Set<Attrezzo>> m = borsa.getContenutoRaggruppatoPerPeso();
+		assertEquals("osso",new ArrayList<Attrezzo>(m.get(1)).get(0).getNome());
+		assertEquals("spada",new ArrayList<Attrezzo>(m.get(2)).get(0).getNome());
+		assertEquals("lancia",new ArrayList<Attrezzo>(m.get(3)).get(0).getNome());
+	}
+	
 }
